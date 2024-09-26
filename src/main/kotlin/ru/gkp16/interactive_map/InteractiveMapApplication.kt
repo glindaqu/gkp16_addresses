8eb31a855d739996cec5ecd375a4f61c6dd77a15
glindaqu <q5481996@gmail.com>
2024-09-26 18:46:01 +0700

    project structure upd

---
+++ src/main/kotlin/ru/gkp16/interactive_map/InteractiveMapApplication.kt
@@ -0,0 +1,13 @@
+package ru.gkp16.interactive_map
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InteractiveMapApplication
+
+fun main(args: Array<String>) {
+	runApplication<InteractiveMapApplication>(*args)
+}
+
+
